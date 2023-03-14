707732d8784263d6cf63b4f952593c59c25101e5
Uriel Arthur MILLOGO <97960262+N0oKAMI@users.noreply.github.com>
2023-03-14 02:05:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hey I am here !");
+
+let zoneSaisie = document.getElementById('zone-saisie');
+let resultat = document.getElementById('resultat');
+
+function display(chiffre) {
+    zoneSaisie.value += chiffre;
+}
+
+function ajouterOperateur(operateur) {
+    zoneSaisie.value += operateur;
+}
+
+function ajouterVirgule() {
+    if (!zoneSaisie.value.includes('.')) {
+        zoneSaisie.value += '.';
+    }
+}
+
+function ajouterParenthese(parenthese) {
+    zoneSaisie.value += parenthese;
+}
+
+function effacer() {
+    zoneSaisie.value = zoneSaisie.value.slice(0, -1);
+}
+
+function calculer() {
+    let expression = zoneSaisie.value;
+    resultat.value = eval(expression);
+}
